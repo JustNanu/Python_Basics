a20db4fcb8d64805b650bd9e2fd9a735fd3ae013
Abhijeet Solanki <118198054+JustNanu@users.noreply.github.com>
2022-12-03 14:25:02 +0530

    Add files via upload

---
+++ vertical_output.py
@@ -0,0 +1,15 @@
+#vertical output to input sentence
+
+sentence = input("Enter a sentence: ")
+
+   #Split a string into a list where each word is a list item
+sentence.split
+
+print(sentence.split())
+list = sentence.split()
+print ("The original list is : " + str(list))
+
+   #join elements of the sequence separated by a string separator
+   #\n prints each letter in a new line
+
+print('\n'.join(list))
